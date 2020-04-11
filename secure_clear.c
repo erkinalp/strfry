9d947121a2d048c0dbe398404049e5650ee5a066
Erkin Alp Güney <erkinalp9035@gmail.com>
2020-04-11 22:51:17 +0300

    Added licence info

--- secure_clear.c
+++ secure_clear.c
@@ -1,3 +1,17 @@
+/*
+  strfry(1) - a stdio randomiser using strfry(3)
+    Copyright (C) 2014  Erkin Alp Güney
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3
+    of the License, or (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <string.h>
 
 void secure_clear(void * data, size_t size) {
